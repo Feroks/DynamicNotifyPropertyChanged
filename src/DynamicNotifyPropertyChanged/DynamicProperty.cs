24893f61772f696b2bf2f86b77bdd80eaf0c0db8
Mihails Popovs <mp@best.lv>
2021-08-26 21:24:58 +0300

    Added additional constructor for DynamicProperty that allow setting which events should be fired

--- src/DynamicNotifyPropertyChanged/DynamicProperty.cs
+++ src/DynamicNotifyPropertyChanged/DynamicProperty.cs
@@ -9,10 +9,22 @@ namespace DynamicNotifyPropertyChanged
 	{
 		/// <param name="name">Name of the property.</param>
 		/// <param name="type">Type of the property.</param>
-		public DynamicProperty(string name, Type type)
+		public DynamicProperty(string name, Type type) : this(name, type, true, true)
 		{
 			Name = name;
 			Type = type;
+		}
+
+		/// <param name="name">Name of the property.</param>
+		/// <param name="type">Type of the property.</param>
+		/// <param name="raisePropertyChanged">True, if <see cref="BaseNotifyPropertyChangedClass.PropertyChanged"/> should be triggered.</param>
+		/// <param name="raisePropertyChanging">True, if <see cref="BaseNotifyPropertyChangedClass.PropertyChanging"/> should be triggered.</param>
+		public DynamicProperty(string name, Type type, bool raisePropertyChanged, bool raisePropertyChanging)
+		{
+			Name = name;
+			Type = type;
+			RaisePropertyChanged = raisePropertyChanged;
+			RaisePropertyChanging = raisePropertyChanging;
 		}
 
 		/// <summary>
@@ -24,5 +36,15 @@ namespace DynamicNotifyPropertyChanged
 		/// Get <see cref="Type"/> of the property.
 		/// </summary>
 		public Type Type { get; }
+
+		/// <summary>
+		/// Get if <see cref="BaseNotifyPropertyChangedClass.PropertyChanged"/> should be triggered after property value is changed.
+		/// </summary>
+		public bool RaisePropertyChanged { get; }
+
+		/// <summary>
+		/// Get if <see cref="BaseNotifyPropertyChangedClass.PropertyChanging"/> should be triggered before property value is changed.
+		/// </summary>
+		public bool RaisePropertyChanging { get; }
 	}
 }
