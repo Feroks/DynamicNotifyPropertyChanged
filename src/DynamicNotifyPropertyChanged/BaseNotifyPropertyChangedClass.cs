fcfec5f859c200c1dd3dd7180f51a52fe1c154f9
Mihails Popovs <mp@best.lv>
2021-08-26 21:25:53 +0300

    Implement INotifyPropertyChanged in BaseNotifyPropertyChangedClass

--- src/DynamicNotifyPropertyChanged/BaseNotifyPropertyChangedClass.cs
+++ src/DynamicNotifyPropertyChanged/BaseNotifyPropertyChangedClass.cs
@@ -4,11 +4,18 @@ using System.Runtime.CompilerServices;
 namespace DynamicNotifyPropertyChanged
 {
 	/// <summary>
-	/// Empty class that implements <see cref="INotifyPropertyChanged"/>.
+	/// Empty class that implements <see cref="INotifyPropertyChanged"/> and <see cref="INotifyPropertyChanged"/>.
 	/// </summary>
-	public abstract class BaseNotifyPropertyChangedClass : INotifyPropertyChanged
+	public abstract class BaseNotifyPropertyChangedClass : INotifyPropertyChanging, INotifyPropertyChanged
 	{
+		public event PropertyChangingEventHandler? PropertyChanging;
+
 		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+		}
 
 		protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 		{
