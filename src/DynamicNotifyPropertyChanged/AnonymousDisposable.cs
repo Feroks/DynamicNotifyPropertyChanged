8b59488ee9463b9570eb9e2446235603c1918bcb
Mihails Popovs <mp@best.lv>
2021-08-26 22:08:24 +0300

    Added AnonymousDisposable

---
+++ src/DynamicNotifyPropertyChanged/AnonymousDisposable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DynamicNotifyPropertyChanged
+{
+	internal class AnonymousDisposable : IDisposable
+	{
+		private readonly Action _action;
+
+		internal AnonymousDisposable(Action action)
+		{
+			_action = action;
+		}
+
+		public void Dispose()
+		{
+			_action();
+		}
+	}
+}
